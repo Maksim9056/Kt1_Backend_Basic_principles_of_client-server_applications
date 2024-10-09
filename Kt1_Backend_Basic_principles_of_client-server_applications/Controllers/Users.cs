1029d069db681ca53eee16742c4f6b7ed2385ae3
Maksim9056 <bobreczovm@inbox.ru>
2024-10-09 18:27:55 +0300

    Правки

---
+++ Kt1_Backend_Basic_principles_of_client-server_applications/Controllers/Users.cs
@@ -0,0 +1,34 @@
+﻿using Kt1_Backend_Basic_principles_of_client_server_applications.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Kt1_Backend_Basic_principles_of_client_server_applications.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private static List<User> users = new List<User>();
+
+        //public UsersController()
+        //{
+        //}
+
+        // Инициализация списка пользователей
+
+        // Метод для обработки GET-запроса
+        [HttpGet("getusers")]
+        public List<User> GetUsers()
+        {
+            return users;
+        }
+
+        // Метод для обработки POST-запроса
+        [HttpPost]
+        public IActionResult PostUser([FromBody] User data)
+        {
+            users.Add(data);
+            return Ok($"POST-запрос успешно обработан! Данные: {data.Name}, {data.Password}");
+        }
+    }
+}
---
+++ Kt1_Backend_Basic_principles_of_client-server_applications/Model/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Kt1_Backend_Basic_principles_of_client_server_applications.Model
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public User(long id, string name, string password)
+        {
+            Id = id;
+            Name = name;
+            Password = password;
+        }
+        public User() { }
+
+    }
+}
